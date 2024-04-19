5496370cd3c7ec2da695094541d1bb31364d86c5
rdinnager <r.dinnage@gmail.com>
2024-04-19 11:22:58 -0400

    minor changes

--- R/squamate_test.R
+++ R/squamate_test.R
@@ -3,5 +3,9 @@ library(sf)
 library(ape)
 
 squamates <- st_read("data/maps/squamates/squamates")
-tree <- read.tree("data/phylogenies/squamates/squam_shl_new_Posterior_9755.5000.trees")[[101]]
-write.tree(tree, "data/phylogenies/squamates/hap_herps.tre")
+#tree <- read.tree("data/phylogenies/squamates/squam_shl_new_Posterior_9755.5000.trees")[[101]]
+#write.tree(tree, "data/phylogenies/squamates/squamates.tre")
+
+tree <- read.tree("data/phylogenies/squamates/squamates.tre")
+
+n_intree <- sum(gsub(" ", "_", squamates$Binomial) %in% tree$tip.label)
